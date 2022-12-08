2f46bc5ebb7a5f7b243000d5530823c093ac2ba6
lrl <742798240@qq.com>
2022-12-09 00:40:08 +0800

    feat: 添加守卫示例

---
+++ src/guard/role/role.guard.ts
@@ -0,0 +1,26 @@
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import type { Request } from 'express';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+/**
+ * 在地址栏输入 - http://localhost:3000/guard?role=admin 请求通过
+ * 在地址栏输入 - http://localhost:3000/guard?role=111 请求被守卫拦截，返回403
+ */
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private Reflector: Reflector) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    /** 获取接口元信息 */
+    const admin = this.Reflector.get<string[]>('role', context.getHandler());
+    /** 获取请求的参数 */
+    const req = context.switchToHttp().getRequest<Request>();
+    /** 如果请求参数符合元信息要求，通过守卫 */
+    if (admin.includes(req.query.role as string)) {
+      return true;
+    }
+    return false;
+  }
+}
